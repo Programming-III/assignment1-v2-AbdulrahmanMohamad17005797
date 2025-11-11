92c322b0f1293b0989d26fdca8125966fea43261
AbdulrahmanMohamad17005797 <zentheunworthy@gmail.com>
2025-11-11 09:42:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@ using namespace std;
 
 
 
-
 // ==================== Student Class Implementation ====================
 
 
@@ -41,8 +40,16 @@ using namespace std;
 
 
 // ==================== Main Function ====================
-int main() {
-   
-    
+int main(){
+    Student s = ("Omar Nabil",2202,2, "Informatics");
+    Instructor i= ("Lina Khaled",,"Computer Science",5);
+    Course c = ("CS101", "Introduction to Programming", 3);
+    Student b = ("max",3303,3, "Business");
+    addstudent (b);
+    displaycourseinfo (c);
+    cout << "Instructor info" << endl;
+    display (i);
+    cout << "Student info" << endl;
+    display (s)
     return 0;
 }
