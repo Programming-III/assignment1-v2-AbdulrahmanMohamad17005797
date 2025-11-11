a374a021cf2e44b48eace09180f890cce4d24be1
AbdulrahmanMohamad17005797 <zentheunworthy@gmail.com>
2025-11-11 09:01:06 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,20 +1,19 @@
-#ifndef PERSON_H
-#define PERSON_H
-
+#include <iostream>
 #include <string>
 using namespace std;
 
-//#write Person class here
-
-
-
-
-
-
-
-
-
-
-
-
-#endif
+class Person {
+private:
+    string name;
+    int id;
+public:
+    Person(string n, int i);
+    void setName( string n);
+    void setId(int i);
+    string getName();
+    int getId();
+    void display(){
+        cout << "Name: " << name << endl;
+        cout << "ID: " << id << endl;
+    }
+};
